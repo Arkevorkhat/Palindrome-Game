025366fffadfb65859e4d0f52002bf1d407a4f25
Arkevorkhat <Arkevorkhat@outlook.com>
2018-03-25 16:07:01 -0700

    Saved all files

--- backEnd/BootSeq.java
+++ backEnd/BootSeq.java
@@ -1,5 +1,16 @@
 package backEnd;
 
+import communications.CommLink;
+import users.*;
+
 public class BootSeq {
-	
+	public static boolean boot() {
+		if (CommLink.children.size()<1) {
+			CommLink.children.add(new Child("Admin", "administrator", 100,13));
+		}
+		if (CommLink.parents.size()<1) {
+			CommLink.parents.add(new Parent(null, null, CommLink.children));
+		}
+		return true;
+	}
 }
