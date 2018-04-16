1373fa86b540f86d8658286e8f2b4e898fe08e63
Arkevorkhat <Arkevorkhat@outlook.com>
2018-04-15 22:51:07 -0700

    message

--- communications/CommLink.java
+++ communications/CommLink.java
@@ -8,7 +8,7 @@ public abstract class CommLink {
 	public static ArrayList<Parent> parents;
 	public static ArrayList<Teacher> teachers;
 	public static ArrayList<Message> messages;
-	public static ArrayList<Administrator> admins;
+	//public static ArrayList<Administrator> admins;
 	@SuppressWarnings("rawtypes")
 	public static ArrayList<ArrayList> people = new ArrayList<ArrayList>();
 	public abstract void init();
--- users/Teacher.java
+++ users/Teacher.java
@@ -1,17 +1,12 @@
 package users;
-
 import communications.CommLink.listable;
-
 import communications.CommLink;
 import communications.Message;
-
-public class Teacher extends Adult implements listable<Teacher> {
+public class Teacher extends Adult implements listable<Teacher> { 
 	private static final long serialVersionUID = -4062870041480934701L;
-
 	public Teacher(String UN, String PW, Student[] chldrn, long CID) {
 		super(UN, PW, chldrn);
 	}
-
 	public Boolean sendMessage(String Message, Person recipient) {
 		try {
 			new Message(Message, this.getUUID(), recipient.getUUID()).addToSet();
@@ -21,28 +16,28 @@ public class Teacher extends Adult implements listable<Teacher> {
 		}
 		return true;
 	}
-
-	public Boolean broadcastMessage(String message) {
-		try {
-			for (Student i : CommLink.students) {
-
-				new Message(message, i.getUUID(), this.getUUID()).addToSet();
-
+	public Message[] getMessages() {
+		
+		return null;
+	}
+	public Boolean broadcastMessage(String message) { //FIXME: add valid return statement.
+		for(Student i : CommLink.students) {
+			try {
+				new Message(message,i.getUUID(),this.getUUID()).addToSet();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			return true;
-		} catch (IllegalAccessException e) {
-			return false;
 		}
+		return null;
 	}
-
 	public void addToSet() {
-		CommLink.teachers.add(this);
+		communications.CommLink.teachers.add(this);
 	}
-
 	@Override
 	public Teacher getFromSet(long ID) throws IllegalArgumentException {
 		for (Teacher i : CommLink.teachers) {
-			if (i.getUUID() == ID) {
+			if(i.getUUID()==ID) {
 				return i;
 			}
 		}
